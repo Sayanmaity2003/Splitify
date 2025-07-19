27fba4b9146ce9236dce09c2321f6e37458b5efd
Sayan Maity <sayanmaity2003@gmail.com>
2025-07-19 23:35:00 +0530

    Shadcn UI installed and tested

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>Welcome to Splitify
+      <br />
+      <Button variant={'destructive'}>Click</Button>
+    </div>
+
+  );
+}
