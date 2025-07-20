f8b3387658afc88ca132c39b440fdfb8f35024f6
Sayan Maity <sayanmaity2003@gmail.com>
2025-07-20 17:16:23 +0530

    Protected Route Middleware created

--- middleware.js
+++ middleware.js
@@ -1,12 +1,37 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+//This are routes are protected by clerkMiddleware-function
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/expenses(.*)",
+  "/contacts(.*)",
+  "/groups(.*)",
+  "/person(.*)",
+  "/settlements(.*)",
+]);
+
+//This is clerkMiddleware-function
+export default clerkMiddleware(async (auth, req) => {
+  //This will give the userId of current loggedIn user
+  const { userId } = await auth();
+  // console.log("auth() result:", userId);
+  //If user is not logged-in and trying to access the above routes then redirect them to sign-in page
+  if (!userId && isProtectedRoute(req)) {
+    const { redirectToSignIn } = await auth();
+    // Redirect to sign-in page 
+    return redirectToSignIn();
+  }
+
+  // If logged-in(userId found) then moveOn
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
