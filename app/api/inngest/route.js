2bab395c8f29c38aa8d202add61b6c45d77909ed
Sayan Maity <sayanmaity2003@gmail.com>
2025-07-20 12:47:04 +0530

    Inngest(For payment reminders and insights of user spendings) setup and tested.

---
+++ app/api/inngest/route.js
@@ -0,0 +1,13 @@
+// This is the route(.../api/inngest) for inngest function
+import { inngest } from "@/lib/inngest/client"; //This is inngest client
+import { helloWorld } from "@/lib/inngest/functions"; //This is function
+import { serve } from "inngest/next";
+
+// Create an API that serves zero functions
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    /* your functions will be passed here later! */
+    helloWorld,
+  ],
+});
